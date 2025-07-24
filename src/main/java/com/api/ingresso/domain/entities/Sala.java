d822b6a7349a99e98e14d3ee1563bbcb14d67f2b
argosmaia <argosantao2013@gmail.com>
2025-07-24 14:46:23 -0300

    feat(pedido): implementar modelo completo de pedido com itens, produto, valor e pagamento
    - Criação da estrutura de entidades Pedido, Item e Produto com relacionamentos corretos
    - Inclusão do enum MetodoPagamento (PIX, Crédito, Débito)
    - Ajuste na estrutura do Produto para permitir lanche e bebida com enum TipoProduto
    - Item representa um produto escolhido no pedido, com quantidade
    - Pedido vinculado a usuário, filme, sala, sessão e valor total
    - Preparação para criação de DTOs (PedidoEntrada, PedidoResposta)
    - Planejamento de serviço para cálculo automático do valor do pedido
    - Validações futuras previstas: quantidade de itens, método de pagamento e consistência de valores

--- src/main/java/com/api/ingresso/domain/entities/Sala.java
+++ src/main/java/com/api/ingresso/domain/entities/Sala.java
@@ -3,9 +3,36 @@
  */
 package com.api.ingresso.domain.entities;
 
+import java.util.UUID;
+
+import com.api.ingresso.domain.embeddable.Endereco;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * 
  */
+@Entity(name = "Sala")
+@Table(name = "salas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class Sala {
-
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    @Embedded
+    private Endereco endereco;
 }
--- src/main/java/com/api/ingresso/domain/entities/Sessao.java
+++ src/main/java/com/api/ingresso/domain/entities/Sessao.java
@@ -3,9 +3,33 @@
  */
 package com.api.ingresso.domain.entities;
 
+import java.util.UUID;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * 
  */
+@Entity(name = "Sessao")
+@Table(name = "sessoes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class Sessao {
-
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private Filme sessao;
+    private Date horario;
 }
